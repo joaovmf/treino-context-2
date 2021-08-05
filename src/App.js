1ecce9153430f28669e4d7c44df0dea734fc157f
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-08-05 20:46:41 -0300

    Theme treino com context

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import FunctionContextComponent from "./FunctionContextComponent";
+import { ThemeProvider } from "./ThemeContext";
+
+function App() {
+  return (
+    <>
+      <ThemeProvider>
+        <FunctionContextComponent />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
